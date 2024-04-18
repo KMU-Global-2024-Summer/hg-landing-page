cd1b8764f514f769db463ce162686a4a3883f1f3
padack2001 <53219315+padack2001@users.noreply.github.com>
2024-04-18 16:03:55 +0000

    마지막 페이지 수정

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tab-link");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    // Set up event listeners and open the first tab
+    document.querySelectorAll('.tab-link').forEach(tab => {
+        tab.addEventListener('click', function(e) {
+            openTab(e, this.getAttribute('data-tabname'));
+        });
+    });
+    document.getElementsByClassName('tab-link')[0].click();
+});
 
-// Get the first tab to display by default
-document.getElementsByClassName('tab-link')[0].click();
